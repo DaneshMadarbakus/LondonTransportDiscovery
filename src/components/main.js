1e5a391a2dd65f45cb0166d22cd0f8a1c090203c
Danesh Madarbakus <daneshplt@hotmail.co.uk>
2018-10-06 19:39:53 +0100

    made succesful api request

--- src/components/main.js
+++ src/components/main.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 import './main.css';
 import Content from './content.js';
 import Menu from './menu.js';
-import {NightService, GoodService, DisruptedService} from './indicators.js';
+import { NightService, GoodService, DisruptedService } from './indicators.js';
 
 class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOptions: [],
+      content: []
+    }
+    this.loadApi = this.loadApi.bind(this);
+  }
+
+  componentDidMount () {
+    this.loadApi();
+  }
+
+  loadApi() {
+    fetch('https://api.tfl.gov.uk/Line/Mode/tube,overground,dlr/Status?detail=true')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+
   render() {
     return (
       <div className="container">
